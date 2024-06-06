f69417250c9e8313f03cc8dd1e573622e6893ec6
arpansahu <arpanrocks95@gmail.com>
2024-06-06 14:09:15 +0530

    renaming

--- great_chat/asgi.py
+++ great_chat/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_starter.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "great_chat.settings")
 
 application = get_asgi_application()
