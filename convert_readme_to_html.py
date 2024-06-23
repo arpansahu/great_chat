a6e7d94fba178da3a34d3e644a3fdc81df18ea8c
arpansahu <arpanrocks95@gmail.com>
2024-06-23 20:24:52 +0530

    a

--- convert_readme_to_html.py
+++ convert_readme_to_html.py
@@ -4,6 +4,26 @@ from markdown.extensions.nl2br import Nl2BrExtension
 from markdown.extensions.extra import ExtraExtension
 from bs4 import BeautifulSoup
 
+# Function to adjust code blocks in the markdown file
+def adjust_code_blocks(input_file, output_file):
+    with open(input_file, 'r') as file:
+        content = file.readlines()
+    
+    adjusted_content = []
+    inside_code_block = False
+    
+    for line in content:
+        if line.lstrip().startswith("```"):
+            inside_code_block = not inside_code_block
+            adjusted_content.append(line.lstrip())
+        elif inside_code_block:
+            adjusted_content.append(line)
+        else:
+            adjusted_content.append(line)
+    
+    with open(output_file, 'w') as file:
+        file.writelines(adjusted_content)
+
 # Function to process the markdown content and convert it to HTML
 def process_markdown_to_html(input_file, intermediate_file, output_file):
     # Adjust code blocks
