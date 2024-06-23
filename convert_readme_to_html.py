06b0e5a7300cdf3653fa7ad99a41b6f806d5524f
arpansahu <arpanrocks95@gmail.com>
2024-06-23 20:23:27 +0530

    a

--- convert_readme_to_html.py
+++ convert_readme_to_html.py
@@ -4,26 +4,6 @@ from markdown.extensions.nl2br import Nl2BrExtension
 from markdown.extensions.extra import ExtraExtension
 from bs4 import BeautifulSoup
 
-# Function to adjust code blocks in the markdown file
-def adjust_code_blocks(input_file, output_file):
-    with open(input_file, 'r') as file:
-        content = file.readlines()
-    
-    adjusted_content = []
-    inside_code_block = False
-    
-    for line in content:
-        if line.lstrip().startswith("```"):
-            inside_code_block = not inside_code_block
-            adjusted_content.append(line.lstrip())
-        elif inside_code_block:
-            adjusted_content.append(line)
-        else:
-            adjusted_content.append(line)
-    
-    with open(output_file, 'w') as file:
-        file.writelines(adjusted_content)
-
 # Function to process the markdown content and convert it to HTML
 def process_markdown_to_html(input_file, intermediate_file, output_file):
     # Adjust code blocks
@@ -39,12 +19,6 @@ def process_markdown_to_html(input_file, intermediate_file, output_file):
     # Parse the HTML content with BeautifulSoup to ensure no alteration to existing HTML tags and attributes
     soup = BeautifulSoup(html_content, 'html.parser')
     
-    # Ensure all <pre><code> tags are correctly formatted
-    for pre in soup.find_all('pre'):
-        if pre.code:
-            code_block = pre.code
-            code_block.string = '\n' + code_block.string.strip() + '\n'
-
     # Write the final HTML content to a new file
     with open(output_file, 'w') as file:
         file.write(soup.prettify(formatter=None))
