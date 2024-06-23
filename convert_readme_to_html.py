36c2f26a6c243b74150d08d9998c66805dd3ebce
arpansahu <arpanrocks95@gmail.com>
2024-06-24 00:12:43 +0530

    added support for local images to be converted into html

--- convert_readme_to_html.py
+++ convert_readme_to_html.py
@@ -1,11 +1,33 @@
 import markdown
 import html
+import configparser
+import os
 from markdown.extensions.fenced_code import FencedCodeExtension
 from markdown.extensions.nl2br import Nl2BrExtension
 from markdown.extensions.extra import ExtraExtension
 from bs4 import BeautifulSoup
 import re
 
+# Function to get the GitHub raw base URL from the .git/config file
+def get_github_raw_base_url():
+    config = configparser.ConfigParser()
+    config.read(os.path.join('.git', 'config'))
+
+    if 'remote "origin"' in config:
+        url = config['remote "origin"']['url']
+        if url.startswith("git@"):
+            # Convert git@github.com:user/repo.git to https://github.com/user/repo
+            url = url.replace("git@", "https://").replace(".com:", ".com/")
+        elif url.startswith("https://"):
+            # Remove .git suffix if present
+            url = url[:-4] if url.endswith(".git") else url
+        
+        # Create the raw GitHub content URL
+        raw_base_url = url.replace("https://github.com", "https://raw.githubusercontent.com")
+        return raw_base_url + "/main"
+    else:
+        raise ValueError("No remote origin found in .git/config")
+        
 # Function to adjust code blocks in the markdown file
 def adjust_code_blocks(input_file, output_file):
     with open(input_file, 'r') as file:
@@ -25,6 +47,22 @@ def adjust_code_blocks(input_file, output_file):
     
     with open(output_file, 'w') as file:
         file.writelines(adjusted_content)
+
+# Function to convert local Markdown image links to raw GitHub content URLs
+def convert_local_image_urls(text):
+    def replace(match):
+        alt_text = match.group(1)
+        url = match.group(2)
+        GITHUB_RAW_BASE_URL =  get_github_raw_base_url()
+
+        # Check if the URL is a local reference (does not contain "http" or "https")
+        if not url.startswith("http"):
+            raw_url = f"{GITHUB_RAW_BASE_URL}/{url.lstrip('/')}"
+            return f'![{alt_text}]({raw_url})'
+        return match.group(0)
+
+    image_pattern = re.compile(r'!\[(.*?)\]\((.*?)\)')
+    return image_pattern.sub(replace, text)
 
 # Function to convert GitHub image URLs to raw GitHub content URLs
 def convert_github_image_urls(text):
@@ -65,6 +103,9 @@ def process_markdown_to_html(input_file, intermediate_file, output_file):
     # Convert GitHub image URLs to raw GitHub content URLs
     readme_text = convert_github_image_urls(readme_text)
     
+    # Convert local image URLs to raw GitHub content URLs
+    readme_text = convert_local_image_urls(readme_text)
+
     # Convert Markdown image links to HTML <img> tags
     readme_text = convert_images_to_html(readme_text)
     
