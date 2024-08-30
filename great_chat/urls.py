432407cd8c393f270b14a76273ed234c8361d1ce
Arpan Sahu <arpanrocks95@gmail.com>
2024-08-30 13:03:53 +0530

    updated time import in url

--- great_chat/urls.py
+++ great_chat/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 from django.conf.urls.static import static
 from django.conf import settings
-
+import time
 
 from account.views import (
     CustomPasswordResetView,
